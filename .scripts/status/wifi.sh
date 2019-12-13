9f77e18c8957c90458315559c8633372b4c5f7fc
jzbor <jzbor@posteo.net>
2019-12-14 00:26:35 +0100

    New polybar wifi + bluetooth indicator

--- .scripts/status/wifi.sh
+++ .scripts/status/wifi.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
-ssid="$(iwgetid -r)"
-localip="$(ip addr | grep "inet " | awk '{print "\t" $2 "\t\t(" $(NF) ")"}')"
-publicip="$(curl ifconfig.me)"
+if [ "$1" = "notify" ]; then
+    ssid="$(iwgetid -r)"
+    localip="$(ip addr | grep "inet " | awk '{print "\t" $2 "\t\t(" $(NF) ")"}')"
+    publicip="$(curl ifconfig.me)"
 
-if [ "$ssid" = "" ]; then
-    ssid="Not Connected"
+    if [ "$ssid" = "" ]; then
+        ssid="Not Connected"
+    fi
+
+    dunstify -a "Network Info" "Wifi: $ssid
+
+    Local:
+    $localip
+
+    Public:
+        $publicip"
+else
+    if wifi | grep -q "on"; then
+	echo 直
+    else
+	echo 睊
+    fi
 fi
 
-dunstify -a "Network Info" "Wifi: $ssid
-
-Local:
-$localip
-
-Public:
-    $publicip"
