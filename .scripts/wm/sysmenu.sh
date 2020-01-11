925e69c590dccb4a2f77f5393e5347bca1cfaa76
jzbor <jzbor@posteo.net>
2020-01-12 00:40:16 +0100

    Added bspc quit to sysmenu

--- .scripts/wm/sysmenu.sh
+++ .scripts/wm/sysmenu.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# @TODO Filter out names at the beginning before piping to bash
 sed 's/ |.*//g' < "$HOME"/.config/rofi/sysmenu.txt | \
     rofi -dmenu -i -p "System" | \
     xargs -I {} grep "{}" "$HOME"/.config/rofi/sysmenu.txt | \
