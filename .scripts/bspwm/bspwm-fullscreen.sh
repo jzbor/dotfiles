ce577148e467c7ab81350dede8cb6f2d71e767d8
jzbor <jzbor@posteo.net>
2020-01-20 23:21:22 +0100

    Latest changes for bspwm config

--- .scripts/bspwm/bspwm-fullscreen.sh
+++ .scripts/bspwm/bspwm-fullscreen.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+fullscreened_moved () {
+    event_type="$1"
+    src_monitor="$2"
+    dst_monitor="$5"
+    src_monitor_name=$(bspc query -m $src_monitor -M --names)
+    dst_monitor_name=$(bspc query -m $dst_monitor -M --names)
+
+
+}
+
 handle_event () {
     event_type="$1"
     monitor="$2"
     desktop="$3"
+    node="$4"
     layout="$5 $6"
     monitor_name=$(bspc query -m $monitor -M --names)
 
@@ -11,6 +22,7 @@ handle_event () {
 	"fullscreen on")
 	    #xdo lower -n polybar
 	    xdo hide -a polybar-$monitor_name
+
 	    ;;
 	"fullscreen off")
 	    #xdo raise -n polybar
