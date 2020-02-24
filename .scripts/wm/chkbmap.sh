f26e82b64aa0e04132a3abdd280325a79c058685
jzbor <jzbor@posteo.net>
2020-02-24 12:53:33 +0100

    Latest script updates

--- .scripts/wm/chkbmap.sh
+++ .scripts/wm/chkbmap.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
+german_layout="de"
+#german_layout="de -variant querty"
+
+if [ "$1" = "init" ]; then
+    setxkbmap $german_layout
+    exit
+fi
 
 old_layout="$(setxkbmap -query | grep layout | awk '{ print$2 }')"
 
@@ -7,13 +14,15 @@ case $old_layout in
 	new_layout="us"
 	;;
     "us" | "gb")
-	new_layout="de"
+	new_layout="$german_layout"
 	;;
     *)
 	dunstify -a Keyboard "No switching behaviour set for $old_layout"
+	exit 1
 	;;
 esac
 
 dunstify -a Keyboard "$old_layout -> $new_layout"
+# The word splitting is intended
 setxkbmap $new_layout
 
