3284ddef0feaf1497bcd9173e9d0ff183132ed65
jzbor <jzbor@posteo.net>
2020-03-13 22:34:29 +0100

    Improved sysmenu script

--- .scripts/de/sysmenu.sh
+++ .scripts/de/sysmenu.sh
@@ -3,5 +3,6 @@
 # @TODO Filter out names at the beginning before piping to bash
 sed 's/ |.*//g' < "$HOME"/.config/rofi/sysmenu.txt | \
     rofi -dmenu -i -p "System" | \
-    xargs -I {} grep "{}" "$HOME"/.config/rofi/sysmenu.txt | \
+    xargs -I {} grep "^{}\s*|" "$HOME"/.config/rofi/sysmenu.txt | \
+    sed 's/^.*|\s*//' | \
     /bin/bash
