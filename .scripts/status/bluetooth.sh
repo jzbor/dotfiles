8488146fbd3133143cef6c5fbca2bfea313e9b0d
jzbor <jzbor@posteo.net>
2020-03-14 13:48:04 +0100

    New scripts for toggling wifi and bluetooth

--- .scripts/status/bluetooth.sh
+++ .scripts/status/bluetooth.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
 
-if bluetooth | grep -q "on"; then
-    echo 
+if [ "$#" = "0" ]; then
+    if bluetooth | grep -q "on"; then
+	echo 
+    else
+	echo 
+    fi
 else
-    echo 
+    case "$1" in
+	"toggle")
+	    device_query="$(rfkill --output "ID,TYPE,SOFT,HARD" | \
+		grep "bluetooth" | head -n 1 | \
+		sed 's/^\s//g;s/\s\+/ /g')"
+	    dev_id="$(echo "$device_query" | cut -d " " -f 1)"
+	    dev_soft="$(echo "$device_query" | cut -d " " -f 3)"
+
+	    if [ "$dev_soft" = "unblocked" ]; then
+		sudo rfkill block "$dev_id"
+	    else
+		sudo rfkill unblock "$dev_id"
+	    fi
+	    # if (rfkill --output-all -n | grep "tpacpi_bluetooth_sw" > /dev/null); then; fi
+
+	    ;;
+	"*")
+	    ;;
+    esac
 fi
 
 # On but not connected 
--- .scripts/status/wifi.sh
+++ .scripts/status/wifi.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 
-if [ "$1" = "notify" ]; then
-    ssid="$(iwgetid -r)"
-    localip="$(ip addr | grep "inet " | awk '{print "\t" $2 "\t\t(" $(NF) ")"}')"
-    publicip="$(curl ifconfig.me)"
-
-    if [ "$ssid" = "" ]; then
-        ssid="Not Connected"
-    fi
-
-    dunstify -a "Network Info" "Wifi: $ssid
-
-    Local:
-    $localip
-
-    Public:
-        $publicip"
-else
+if [ "$#" = "0" ]; then
     if iwgetid > /dev/null; then
 	echo 直
     elif wifi | grep -q "on"; then
@@ -24,6 +8,39 @@ else
     else
 	echo 
     fi
+else
+    case "$1" in
+	"notify")
+	    ssid="$(iwgetid -r)"
+	    localip="$(ip addr | grep "inet " | awk '{print "\t" $2 "\t\t(" $(NF) ")"}')"
+	    publicip="$(curl ifconfig.me)"
+
+	    if [ "$ssid" = "" ]; then
+		ssid="Not Connected"
+	    fi
+
+	    dunstify -a "Network Info" "Wifi: $ssid
+
+	    Local:
+	    $localip
+
+	    Public:
+		$publicip"
+	    ;;
+	"toggle")
+	    device_query="$(rfkill --output "ID,TYPE,SOFT,HARD" | \
+		grep "wlan" | head -n 1 | \
+		sed 's/^\s//g;s/\s\+/ /g')"
+	    dev_id="$(echo "$device_query" | cut -d " " -f 1)"
+	    dev_soft="$(echo "$device_query" | cut -d " " -f 3)"
+
+	    if [ "$dev_soft" = "unblocked" ]; then
+		sudo rfkill block "$dev_id"
+	    else
+		sudo rfkill unblock "$dev_id"
+	    fi
+	    ;;
+    esac
 fi
 
 # On but not connected: 睊
