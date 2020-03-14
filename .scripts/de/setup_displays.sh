4b844a586d382b937256db8156198bc6e694cb2c
jzbor <jzbor@posteo.net>
2020-03-14 16:34:20 +0100

    Adding script for display management with xrandr

---
+++ .scripts/de/setup_displays.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+connected="$(xrandr | grep " connected " | cut -d " " -f 1)"
+model_info="$(inxi -M | head -n 1)"
+
+for display in $connected; do
+    if [ "$display" = "eDP1" ] \
+	&& echo "$model_info" | grep "ThinkPad T440" > /dev/null \
+	&& grep closed /proc/acpi/button/lid/LID/state > /dev/null ; then
+	echo "Disabling $display (closed lid)"
+	xrandr --output "$display" --off
+    elif [ -z "$last" ]; then
+	echo "Setting up first display: $display"
+	xrandr --output "$display" --auto
+	last="$display"
+    else
+	echo "Setting up $display next to $display"
+	xrandr --output "$display" --auto --right-of "$last"
+    fi
+done
+
+if pgrep polybar > /dev/null; then
+    echo
+    echo "Restarting polybar"
+    "$HOME"/.config/polybar/launch.sh > /dev/null
+fi
