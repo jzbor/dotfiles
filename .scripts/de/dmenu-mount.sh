c7540c8c94dfd7eee01d116f5d7298267a06b2e1
jzbor <jzbor@posteo.net>
2020-03-26 20:33:18 +0100

    Added shortcuts form mounting and unmounting

--- .scripts/de/dmenu-mount.sh
+++ .scripts/de/dmenu-mount.sh
@@ -1,1 +1,26 @@
-lsblk -pln -o NAME,FSTYPE,SIZE | rofi -dmenu
+#! /bin/sh
+# @TODO Use findmnt for unmounting
+
+case $1 in
+    "mnt")
+	operation="mount"
+	title="Mount"
+	;;
+    "umnt")
+	operation="unmount"
+	title="Unmount"
+	;;
+    "*" | "")
+	echo "Option $1 not available!"
+	exit 1
+	;;
+esac
+
+device=$(lsblk -pln -o SIZE,LABEL,NAME | \
+    rofi -dmenu -p "$title" | \
+    sed 's/.*\s\w*//g')
+
+response="$(udisksctl "$operation" --block-device "$device" 2>&1)"
+dunstify -a "$title" "$response"
+
+
