5701dfd6701ac280941003b7ce3328a87a565b3f
jzbor <jzbor@posteo.net>
2020-04-04 23:39:48 +0200

    Adding terminal script for compatibility

---
+++ .scripts/de/terminal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for terminal in "$TERMINAL" xfce4-terminal kitty uxterm xterm x-terminal-emulator urxvt rxvt termit terminator Eterm aterm gnome-terminal roxterm termite lxterminal mate-terminal terminology st qterminal lilyterm tilix terminix konsole guake tilda alacritty hyper; do
+    if command -v "$terminal" > /dev/null 2>&1; then
+	exec "$terminal" "$@"
+    fi
+done
+
+dunstify -a Terminal "There was no compatible terminal found"
