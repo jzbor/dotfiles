b1abf3e0f3e1dfa8ac9bcef04e0706c03da5e819
jzbor <jzbor@posteo.net>
2020-04-07 13:49:42 +0200

    New rofi theme

---
+++ .scripts/de/gen-rofi-colors.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+get_xr () {
+    xrdb -query -all | grep "$1": | sed "s/.*#/#/g" | tail -n1
+}
+
+color_file="$HOME/.config/rofi/colors.rasi"
+
+printf "* {\n" > $color_file
+printf "\tbackground:\t\t%s;\n" $(get_xr themeBackground) >> $color_file
+printf "\tbackground-trans:\t\t%s;\n" $(get_xr themeBackgroundTrans) >> $color_file
+printf "\tforeground:\t\t%s;\n" $(get_xr themeForeground) >> $color_file
+printf "\turgent:\t\t%s;\n" $(get_xr themeUrgent) >> $color_file
+printf "\tselected:\t\t%s;\n" $(get_xr themeSelector) >> $color_file
+printf "}\n" >> $color_file
