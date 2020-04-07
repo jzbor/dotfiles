6e59b93aa1594bf74aa63d127896ac712bf07210
jzbor <jzbor@posteo.net>
2020-04-07 13:55:35 +0200

    Adding new battery theme

--- .scripts/status/battery.sh
+++ .scripts/status/battery.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 get_icon () {
-    let "step = $2 / 5"
-    for i in 4 3 2 1 0 ; do
-	let "level = $step * i"
-	if [ $1 -gt $level ]; then
-	    case $i in
+    case $3 in
+	Charging)
+	    echo 
+	    ;;
+	Discharging | "Not charging" | Unknown)
+	    let "level = $1 / (100 / 5)"
+	    case $level in
 		0)
 		    echo 
 		    ;;
@@ -21,17 +23,25 @@ get_icon () {
 		4) echo
 		    echo 
 		    ;;
+		"*" | "")
+		    echo $level
+		    ;;
 	    esac
 	    return
-	fi
-    done
+	    ;;
+	"*" | "")
+	    echo $3
+	    ;;
+    esac
 }
 
-status_string=""
 for battery in $(eval echo /sys/class/power_supply/BAT?); do
     capacity=$(cat $battery/capacity)
     charge_stop_threshold=$(cat $battery/charge_stop_threshold)
-    status_string=$(printf "%s %s %s" $status_string $(get_icon $capacity $charge_stop_threshold) $capacity%)
+    bat_status=$(cat $battery/status)
+    printf "%s  %s " $(get_icon $capacity $charge_stop_threshold $bat_status) $capacity%
 done
 
-echo $status_string
+for other in $(ls /sys/class/power_supply/ | grep -v 'BAT\?'); do
+    [ "$(cat /sys/class/power_supply/$other/online)" -eq 1 ] && printf " " || printf ""
+done
