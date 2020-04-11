50d6ed925585cede09861103fe2502f63c36164c
jzbor <jzbor@posteo.net>
2020-04-11 19:13:54 +0200

    New menu script

--- .scripts/de/sysmenu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# @TODO Filter out names at the beginning before piping to bash
-sed 's/ |.*//g' < "$HOME"/.config/rofi/sysmenu.txt | \
-    rofi -dmenu -i -p "System" | \
-    xargs -I {} grep "^{}\s*|" "$HOME"/.config/rofi/sysmenu.txt | \
-    sed 's/^.*|\s*//' | \
-    /bin/bash
---
+++ .scripts/dmenu/menu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    file="$HOME"/.config/rofi/main.txt
+else
+    file="$HOME"/.config/rofi/"$1".txt
+fi
+
+delimiter="|"
+
+tail -n +2 "$file" | sed "s/$delimiter.*//" | \
+    rofi -dmenu -p "$(eval echo $(head -n 1 "$file"))" | \
+    xargs -I {} grep "^{}\s*$delimiter" "$file" | \
+    sed "s/^.*$delimiter\s*//" | \
+    /bin/bash
+
