5ca528bb02845ccfe43e834e6cc52534bea0fed5
jzbor <jzbor@posteo.net>
2020-04-14 19:01:26 +0200

    Adding screenshot script

---
+++ .scripts/de/scrot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+case "$1" in
+    "" | select)
+	scrot -s -f 'Screenshot_%Y-%m-%d_%H-%M-%S.png' \
+	    -e 'mv -f $f ~/Pictures/Screenshots/ && dunstify -a Scrot -i ~/Pictures/Screenshots/$f "Sucessfully taken screenshot"'
+	;;
+    focused)
+	scrot -u 'Screenshot_%Y-%m-%d_%H-%M-%S.png' \
+	    -e 'mv -f $f ~/Pictures/Screenshots/ && dunstify -a Scrot -i ~/Pictures/Screenshots/$f "Sucessfully taken screenshot"'
+	;;
+    screen)
+	scrot 'Screenshot_%Y-%m-%d_%H-%M-%S.png' \
+	    -e 'mv -f $f ~/Pictures/Screenshots/ && dunstify -a Scrot -i ~/Pictures/Screenshots/$f "Sucessfully taken screenshot"'
+	;;
+esac
