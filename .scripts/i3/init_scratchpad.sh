dac4f5cbe381cd9e2229e2c5ded0e24f6276d5be
jzbor <jzbor@posteo.net>
2020-04-22 00:57:32 +0200

    Improving the scratchpad script

--- .scripts/i3/init_scratchpad.sh
+++ .scripts/i3/init_scratchpad.sh
@@ -2,7 +2,4 @@
 
 command -v kitty && \
     ( nice -n -9 kitty -T scratchterm &
-	nice -n -8 kitty -T taskmgr -e htop &
-	sleep 2.0;
-	i3-msg move scratchpad;
-	i3-msg move scratchpad) &> $HOME/debug
+	nice -n -8 kitty -T taskmgr -e htop & )
