afc436add743f2bdec445d9258358e9d05298bb6
jzbor <jzbor@posteo.net>
2020-06-20 00:02:10 +0200

    New script to find duplicate files

---
+++ .scripts/documents/duplicates.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# From https://www.reddit.com/r/linuxquestions/comments/ham5i6/find_duplicate_files/
+
+find "$1" -type f -print0 |
+    xargs -0 stat -c "%012s"$'\t'"%n" |
+    sort -k1,1n |
+    uniq -w 12 -D |
+    cut -f2 |
+while read f
+do
+    md5sum "$f"
+done | sort | uniq -w 32 --all-repeated=separate
