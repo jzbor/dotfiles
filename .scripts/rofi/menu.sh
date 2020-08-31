19904312da612694cace765ab7a0da768634a7a7
jzbor <jzbor@posteo.net>
2020-08-31 21:01:43 +0200

    Switching the divider of menu scripts

--- .scripts/rofi/menu.sh
+++ .scripts/rofi/menu.sh
@@ -6,7 +6,7 @@ else
     file="$HOME"/.config/assets/menu/"$1".txt
 fi
 
-delimiter="|"
+delimiter="§"
 
 tail -n +2 "$file" | sed "s/$delimiter.*//" | \
     rofi -dmenu -i -p "$(eval echo $(head -n 1 "$file"))" | \
