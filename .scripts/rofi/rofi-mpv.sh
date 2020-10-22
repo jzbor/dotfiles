af5610789a07fd9361df367656cda58d7534a1d1
jzbor <jzbor@posteo.net>
2020-10-22 19:34:49 +0200

    Properly implementing rofi-mpv menu

--- .scripts/rofi/rofi-mpv.sh
+++ .scripts/rofi/rofi-mpv.sh
@@ -1,6 +1,25 @@
-# !/bin/sh
+#!/bin/sh
 
-url="$(cat ~/.config/assets/web-bookmarks.txt | rofi -dmenu -p "URL: ")"
+dunstify_id="6661"
+hist_file="/tmp/rofi-mpv-history"
+url="$(cat ~/.config/assets/mpv-bookmarks.txt "$hist_file" | rofi -dmenu -p "URL: ")"
+
+# clipboard implementation
+case $url in clip | clipboard)
+    url="$(clipit -c)"
+    ;;
+esac
+
 [ -z "$url" ] && exit
 echo "$url" | grep "https://\|http://" || url="https://$url"
-mpv --idle=once "$url"
+
+if youtube-dl -s "$url" --playlist-items 1; then
+    dunstify -r "$dunstify_id" -a mpv "$(printf "Loading URL:\n%s" "$url")"
+    sleep 1
+    dunstify --close "$dunstify_id"
+else
+    dunstify -r "$dunstify_id" -a mpv "$(printf "Unable to find a video under the given url:\n%s" "$url")"
+fi &
+
+mpv --idle=once "$url" \
+    && ( echo "$url"; cat "$hist_file" ) | gawk '!a[$0]++' > "$hist_file.sec" && cp "$hist_file.sec" "$hist_file"
