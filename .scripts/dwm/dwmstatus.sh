ba1078ba70634b980d9ef105cd0eb0003130fba0
jzbor <jzbor@posteo.net>
2020-12-04 12:25:03 +0100

    Minor changes

--- .scripts/dwm/dwmstatus.sh
+++ .scripts/dwm/dwmstatus.sh
@@ -4,7 +4,14 @@ get_status () {
     extra="$(extra-tray.sh)"
     [ -z "$extra" ] || extra="$extra | "
     music.sh has-player && music="$(music.sh status-small) | "
-    printf "$extra\x02$music\x03$(volume.sh)% |\x04 $(ethernet.sh) $(wifi.sh) $(bluetooth.sh) |\x05  $(date +%R) ||"
+
+	if [ -d /proc/acpi/button/lid ]; then
+		device=" "
+	else
+		device=""
+	fi
+
+    printf "$extra\x02$music\x03$(volume.sh)% |\x04 $(ethernet.sh) $(wifi.sh) $(bluetooth.sh) |\x05  $(date +%R) || \x06$device"
 }
 
 kill -9 $(pgrep $(basename $0) | grep -v $$) 2> /dev/null
--- .scripts/dwm/picom-guardian.sh
+++ .scripts/dwm/picom-guardian.sh
@@ -5,6 +5,7 @@ killall picom; picom --experimental-backends 2> ~/.cache/picom.log
 while ! picom --experimental-backends 2> ~/.cache/picom.log; do
     echo "Picom seems to have crashed; Restarting picom..."
     dunstify -a "Picom Guardian" "Picom seems to have crashed; Restarting picom..."
+	cp ~/.cache/picom.log ~/.cache/picom.log.old
     sleep 2
 done
 
