7ac0bd0d0c3c5cfbe704b3a1973973ef6e03cd92
jzbor <jzbor@posteo.net>
2020-12-29 02:10:23 +0100

    Improving bluetooth script

--- .scripts/status/bluetooth.sh
+++ .scripts/status/bluetooth.sh
@@ -11,29 +11,33 @@ dev_id="$(echo "$device_query" | cut -d " " -f 1)"
 dev_soft="$(echo "$device_query" | cut -d " " -f 3)"
 
 case "$1" in
-    "toggle")
-	if [ "$dev_soft" = "unblocked" ]; then
-	    sudo rfkill block "$dev_id"
-	else
-	    sudo rfkill unblock "$dev_id"
-	fi
-	;;
-    "on")
-	sudo rfkill unblock "$dev_id"
-	;;
-    "off")
-	sudo rfkill block "$dev_id"
-	;;
-    "*")
-	echo Not available!
-	;;
-    "status" | "")
-	if [ "$dev_soft" = "unblocked" ]; then
-	    echo 
-	else
-	    echo 
-	fi
-	;;
+	"toggle")
+		if [ "$dev_soft" = "unblocked" ]; then
+			sudo rfkill block "$dev_id"
+			pkill blueman-applet
+		else
+			sudo rfkill unblock "$dev_id"
+			blueman-applet &
+		fi
+		;;
+	"on")
+		sudo rfkill unblock "$dev_id"
+		blueman-applet &
+		;;
+	"off")
+		sudo rfkill block "$dev_id"
+		pkill blueman-applet
+		;;
+	"*")
+		echo Not available!
+		;;
+	"status" | "")
+		if [ "$dev_soft" = "unblocked" ]; then
+			echo 
+		else
+			echo 
+		fi
+		;;
 esac
 
 # On but not connected 
