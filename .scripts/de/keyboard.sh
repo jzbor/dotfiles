52be442068e88b517168a977514f0b210f30800f
jzbor <jzbor@posteo.net>
2021-01-03 20:53:12 +0100

    Improving keyboard.sh script

--- .scripts/de/keyboard.sh
+++ .scripts/de/keyboard.sh
@@ -6,31 +6,39 @@ command -v checkdeps.sh > /dev/null 2>&1 && . checkdeps.sh
 
 
 options="-option -option caps:escape_shifted_capslock -option lv3:ralt_alt -option lv3:lwin_switch"
-german_layout="de nodeadkeys"
-english_layout="us"
 
-if [ "$1" = "init" ]; then
-    setxkbmap $german_layout
-    exit
+set_layout () {
+	case $1 in
+		de)
+			setxkbmap de nodeadkeys $options
+			;;
+		us)
+			setxkbmap us $options
+			xmodmap -e "keycode 29 = z Z z Z"
+			xmodmap -e "keycode 52 = y Y y Y"
+			;;
+	esac
+}
+
+if [ -z "$1" ]; then
+	old_layout="$(setxkbmap -query | grep layout | awk '{ print$2 }')"
+	case $old_layout in
+		"de" )
+			set_layout us
+			;;
+		"us" | "gb")
+			set_layout de
+			;;
+		*)
+				set_layout de
+			;;
+	esac
+else
+		set_layout "$1"
 fi
 
-old_layout="$(setxkbmap -query | grep layout | awk '{ print$2 }')"
 
-case $old_layout in
-	"de" )
-		new_layout="$english_layout $options"
-		;;
-	"us" | "gb")
-		new_layout="$german_layout $options"
-		;;
-	*)
-		# dunstify -a Keyboard "No switching behaviour set for $old_layout.\nUsing de as fallback."
-		new_layout="$german_layout $options"
-		;;
-esac
 
-# The word splitting is intended
-setxkbmap $new_layout
 dwmstatus-update.sh
 # dunstify -a Keyboard "$old_layout -> $(echo $new_layout | cut -f1 -d' ') ($?)"
 
