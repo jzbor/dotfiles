6d62b9336bd41dc9eb3a0ca101e4273e3b896992
jzbor <jzbor@posteo.net>
2021-01-07 16:11:59 +0100

    Improving keyboard.sh

--- .scripts/de/keyboard.sh
+++ .scripts/de/keyboard.sh
@@ -5,37 +5,44 @@ DEPENDENCIES="dunstify setxkbmap"
 command -v checkdeps.sh > /dev/null 2>&1 && . checkdeps.sh
 
 
-options="-option -option caps:escape_shifted_capslock -option lv3:ralt_alt -option lv3:lwin_switch"
+options="-option -option caps:escape_shifted_capslock -option lv3:ralt_alt"
+default_layout="us"
 
 set_layout () {
     case $1 in
         de)
-            setxkbmap de nodeadkeys $options
+            setxkbmap de nodeadkeys $options -option lv3:lwin_switch
             ;;
         us)
-            setxkbmap us $options
+            setxkbmap us,de $options -option eurosign:e -option grp:lwin_switch
             xmodmap -e "keycode 29 = z Z z Z"
             xmodmap -e "keycode 52 = y Y y Y"
             ;;
     esac
 }
 
-if [ -z "$1" ]; then
-    old_layout="$(setxkbmap -query | grep layout | awk '{ print$2 }')"
-    case $old_layout in
-        "de" )
-            set_layout us
-            ;;
-        "us" | "gb")
-            set_layout de
-            ;;
-        *)
-                set_layout de
-            ;;
-    esac
-else
+case $1 in
+	"toggle")
+		old_layout="$(setxkbmap -query | grep layout | awk '{ print$2 }')"
+		case $old_layout in
+			"de")
+				set_layout us
+				;;
+			"us" | "gb" | "us,de")
+				set_layout de
+				;;
+			*)
+				set_layout "$default_layout"
+				;;
+		esac
+		;;
+	"")
+		set_layout "$default_layout"
+		;;
+	*)
         set_layout "$1"
-fi
+		;;
+esac
 
 
 
