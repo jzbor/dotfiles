1fc7532184a66242081dd6eb3f566b2cb57a5875
jzbor <jzbor@posteo.net>
2021-01-18 17:23:55 +0100

    Allow for audio boost overwriting

--- .scripts/status/volume.sh
+++ .scripts/status/volume.sh
@@ -4,34 +4,51 @@ DEPENDENCIES="dwmstatus-update.sh ffplay pamixer"
 command -v checkdeps.sh > /dev/null 2>&1 && . checkdeps.sh
 
 
+boost_overwrite_file="/tmp/pa-overwrite-boost"
+
 case $1 in
-    "-i" | "--inc" | "inc" | "+")
-	pamixer -i "${2:-5}"
-	ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
-	dwmstatus-update.sh
-	;;
-    "-d" | "--dec" | "dec" | "-")
-	pamixer -d "${2:-5}"
-	ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
-	dwmstatus-update.sh
-	;;
-    "-s" | "--set" | "set" | "=")
-	pamixer --set-volume "$2"
-	dwmstatus-update.sh
-	;;
-    "-g" | "--get" | "get")
-	pamixer --get-volume
-	dwmstatus-update.sh
-	;;
-    "-t" | "--toggle" | "toggle")
-	case $2 in
-	    mute | "") pamixer -t ;;
-	    mic | microphone) pamixer --default-source -t ;;
-	    # Technically no toggle; may change later
-	    mic-unmute | microphone-unmute) pamixer --default-source -u ;;
-	esac
-	dwmstatus-update.sh
-	;;
+	"-i" | "--inc" | "inc" | "+")
+		if [ -f "$boost_overwrite_file" ]; then
+			pamixer -i "${2:-5}" --allow-boost
+		else
+			pamixer -i "${2:-5}"
+		fi
+		ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+		dwmstatus-update.sh
+		;;
+	"-d" | "--dec" | "dec" | "-")
+		if [ -f "$boost_overwrite_file" ]; then
+			pamixer -d "${2:-5}" --allow-boost
+		else
+			pamixer -d "${2:-5}"
+		fi
+		ffplay -nodisp -autoexit /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+		dwmstatus-update.sh
+		;;
+	"-s" | "--set" | "set" | "=")
+		if [ -f "$boost_overwrite_file" ]; then
+			pamixer --set-volume "${2:-5}" --allow-boost
+		else
+			pamixer --set-volume "${2:-5}"
+		fi
+		dwmstatus-update.sh
+		;;
+	"-g" | "--get" | "get")
+		pamixer --get-volume
+		dwmstatus-update.sh
+		;;
+	"-t" | "--toggle" | "toggle")
+		case $2 in
+			mute | "") pamixer -t ;;
+			mic | microphone) pamixer --default-source -t ;;
+			# Technically no toggle; may change later
+			mic-unmute | microphone-unmute) pamixer --default-source -u ;;
+		esac
+		dwmstatus-update.sh
+		;;
+	"-b" | "--boost" )
+		touch "$boost_overwrite_file"
+		;;
 esac
 
 
