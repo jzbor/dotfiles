345c4cb8ac3384a777b3a0289909088eac62e260
jzbor <jzbor@posteo.net>
2021-03-26 18:27:27 +0100

    New modmap feature

--- .local/share/moonwm/autostart.sh
+++ .local/share/moonwm/autostart.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Setup keyboard stuff
-xmodmap -e "keycode 29 = z Z z Z"
-xmodmap -e "keycode 52 = y Y y Y"
-xmodmap -e "keycode 94 = Alt_L Meta_L Alt_L Meta_L"
-
 dunst.sh
 music.sh loop &
 (killall -9 picom-guardian.sh; picom-guardian.sh) &
