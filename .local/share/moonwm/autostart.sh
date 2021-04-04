c5048196fca23a166882bbdf8abc9c605d7245d0
jzbor <jzbor@posteo.net>
2021-04-04 11:46:13 +0200

    New touchegg configuration

--- .local/share/moonwm/autostart.sh
+++ .local/share/moonwm/autostart.sh
@@ -4,3 +4,7 @@ dunst.sh
 music.sh loop &
 (killall -9 picom-guardian.sh; picom-guardian.sh) &
 dwmstatus.sh &
+if [ "$(< /etc/hostname)" = T440 ] && killall -2 touchegg; then
+    touchegg --daemon 750 750 &
+    touchegg &
+fi
