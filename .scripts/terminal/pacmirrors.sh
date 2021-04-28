55d2bc6dc453e4cf532c2539ef10435f57ff783d
jzbor <jzbor@posteo.net>
2021-04-28 15:46:07 +0200

    Improving pacmirrors script

--- .scripts/terminal/pacmirrors.sh
+++ .scripts/terminal/pacmirrors.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-praefix="\e[0;94m==="
+praefix="\n\e[0;94m==="
 suffix="===\e[0m\n"
 
 TEMP_MIRRORLIST="${TEMP:-/tmp}/pacmirrorlist"
-
-print_mirrorlist () {
-    printf "$praefix Your mirrorlist: $suffix\n"
-    cat "$TEMP_MIRRORLIST.fastest"
-}
 
 manjaro () {
     printf "$praefix Running Manjaro's pacman-mirrors $suffix"
@@ -16,34 +11,44 @@ manjaro () {
 }
 
 regular () {
-    printf "$praefix Downloading mirrorlist ($DISTRO) $suffix"
+    printf "$praefix Downloading mirrorlist [$DISTRO] $suffix"
+    echo "Downloading from $MIRRORURL"
     curl -o "$TEMP_MIRRORLIST" "$MIRRORURL" || exit 1
-
-    printf "$praefix Preparing mirrorlist $suffix"
     sed -i 's/#S/S/g' "$TEMP_MIRRORLIST" || exit 1
 
     printf "$praefix Ranking mirrors $suffix"
-    rankmirrors "$TEMP_MIRRORLIST" > "$TEMP_MIRRORLIST.fastest" || exit 1
+    echo "This may take some while..."
+    echo
+    rankmirrors -v "$TEMP_MIRRORLIST" | tee "$TEMP_MIRRORLIST.fastest" || exit 1
 
     printf "$praefix Replace current mirrorlist? $suffix"
     read -p "(y/N) " answer
     case $answer in
         y | Y | yes | Yes) ;;
-        *) print_mirrorlist; exit ;;
+        *) exit ;;
     esac
-    sudo mv -v "$TEMP_MIRRORLIST.fastest" /etc/pacman.d/mirrorlist || exit 1
+    sudo mv -v "$TEMP_MIRRORLIST.fastest" "${DEST:-/etc/pacman.d/mirrorlist}" || exit 1
 }
 
-DISTRO="$(uname -r | sed 's/.*-//')"
-case $DISTRO in
-    MANJARO) manjaro ;;
-    ARCH) MIRRORURL="https://www.archlinux.org/mirrorlist/all"; regular ;;
-    ARTIX) MIRRORURL="https://gitea.artixlinux.org/packagesA/artix-mirrorlist/raw/branch/master/trunk/mirrorlist"; regular ;;
-    *)
-        echo "$DISTRO is currently not supported"
-        exit 1
-        ;;
-esac
+if grep -q 'MANJARO' /proc/version; then
+    DISTRO="Manjaro"
+    manjaro
+elif grep -q 'artixlinux' /proc/version; then
+    DISTRO="Artix"
+    MIRRORURL="https://gitea.artixlinux.org/packagesA/artix-mirrorlist/raw/branch/master/trunk/mirrorlist"
+    regular
+    DISTRO="Artix (Arch mirrors)"
+    MIRRORURL="https://archlinux.org/mirrorlist/all/"
+    DEST="${DEST:-/etc/pacman.d/mirrorlist}-arch"
+    regular
+elif grep -q 'archlinux' /proc/version; then
+    DISTRO="Arch"
+    MIRRORURL="https://archlinux.org/mirrorlist/all/"
+    regular
+else
+    echo "Your distro seems to not be supported"
+    exit 1
+fi
 
 printf "$praefix Update Pacman database and packages $suffix"
 sudo pacman -Syyu
