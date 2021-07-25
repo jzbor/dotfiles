a320cdd971ade035b03ecd02b85dbd45c9c22853
jzbor <jzbor@posteo.net>
2021-07-25 18:31:16 +0200

    Improving wacom script

--- .scripts/de/wacom.sh
+++ .scripts/de/wacom.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-device="$(xsetwacom --list devices | dmenu -l 10 -p "Select device" | sed 's/ *\t.*//')"
+device="$(xsetwacom --list devices | dmenu -i -l 10 -p "Select device" | sed 's/ *\t.*//')"
 [ -z "$device" ] && exit
 output="$(xrandr | grep ' connected ' | sed 's/ (.*//' | \
-    dmenu -l 10 -p "Select monitor to map to" | cut -d' ' -f1)"
+    dmenu -i -l 10 -p "Select monitor to map to" | cut -d' ' -f1)"
 [ -z "$output" ] && exit
 
 xsetwacom --set "$device" MapToOutput "$output"
 
-notify-send -a "Wacom Tablet" "Mapping $device to $output"
