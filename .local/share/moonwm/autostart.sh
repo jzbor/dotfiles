a071ed7e2f4ab4553670ed55c94a081be4e1e810
jzbor <jzbor@posteo.net>
2021-10-08 09:07:59 +0000

    Fixing picom and tar for pbp

--- .local/share/moonwm/autostart.sh
+++ .local/share/moonwm/autostart.sh
@@ -4,7 +4,7 @@ dunst.sh
 music.sh loop &
 if [ "$REGULAR_PICOM" = 1 ]; then
     picom --experimental-backends --config ~/.config/picom-basic.conf
-else
+elif [ "$NOPICOM" != 1]; then
     (killall -9 picom-guardian.sh; picom-guardian.sh) &
 fi
 dwmstatus.sh &
