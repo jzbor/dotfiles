da1cd3cc3f8165a4eb9533e1faeb84a29fe7e036
jzbor <jzbor@posteo.net>
2021-10-12 01:02:30 +0200

    Adding camera detection for PBP to extra tray script

--- .scripts/status/extra-tray.sh
+++ .scripts/status/extra-tray.sh
@@ -4,6 +4,7 @@
 tray=""
 
 [ -f /tmp/.do-not-disturb ] && tray="$tray ﮖ"
+[ "$(< /etc/hostname)" = "pinebook-pro" ] && ! [ -d "/sys/devices/platform/fe3c0000.usb/usb1/1-1/1-1.2" ] && tray="$tray 﫞"
 pamixer --default-source --get-mute > /dev/null && tray="$tray "
 xset q | grep 'Caps Lock:\s*on' > /dev/null && tray="$tray 祝"
 
