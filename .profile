df2c382033cc03c854284a4bcece15938c3ff3fc
jzbor <jzbor@posteo.net>
2022-01-19 23:22:46 +0100

    Recent changes

--- .profile
+++ .profile
@@ -5,7 +5,7 @@ export VISUAL="$EDITOR"
 export BROWSER="firefox"
 export READER="zathura"
 export FILEMANAGER="pcmanfm"
-export DMENUCMD="wmc-utils drun"
+# export DMENUCMD="wmc-utils drun"
 # export LANG="en_US.UTF-8"
 
 if command -v buttermilk > /dev/null; then
--- .scripts/dwm/spotify.sh
+++ .scripts/dwm/spotify.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 
-if pidof spotify; then
-    moonctl toggleview $((1<<7));
+if pidof spotify > /dev/null; then
+    for wid in $(xdotool search --class spotify | tr '\n' ' '); do
+        moonctl activate "$wid"
+    done
 else
     spotify &
-    sleep 0.5
-    inittags="$(moonctl currenttags)"
-    for xid in $(xdotool search --class spotify); do
-        moonctl activate "$xid" 200 && moonctl tag $((1<<7))
+    while [ -z "$xid" ]; do
+        for xid in $(xdotool search --class spotify); do
+            moonctl activate "$xid" 200 && moonctl tag $((1<<7))
+        done
+        sleep 0.2
     done
-    moonctl view "$inittags"
 fi
